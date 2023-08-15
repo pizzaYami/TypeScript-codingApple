07955c95b5129f72ed9b18aeee45203f1cedce24
pizzaYami <cyj9407@naver.com>
2023-08-15 19:22:47 +0900

    type의 여러예시

---
+++ index.js
@@ -0,0 +1,20 @@
+var 문자 = "kim";
+var 숫자 = 50;
+var 참거짓 = true;
+var 배열1 = [1, 2, 3];
+var 배열2 = ["a", "b", "c"];
+var 배열3 = [1, "a", 2, "b"];
+var 객체1 = { 사과: "apple", 포도: "grape" };
+var 객체2 = { age: 14, koreaAge: 16 };
+var 이름 = "조용주";
+var 나이 = 29;
+var 출생지역 = "경기도 이천";
+var favoriteSong = {
+    singer: "장범준",
+    song: "고백",
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,29 @@
+let 문자: string = "kim";
+let 숫자: number = 50;
+let 참거짓: boolean = true;
+
+let 배열1: number[] = [1, 2, 3];
+let 배열2: string[] = ["a", "b", "c"];
+let 배열3: (number | string)[] = [1, "a", 2, "b"];
+
+let 객체1: { 사과: string; 포도: string } = { 사과: "apple", 포도: "grape" };
+let 객체2: { age: number; koreaAge: number } = { age: 14, koreaAge: 16 };
+
+let 이름: string = "조용주";
+let 나이: number = 29;
+let 출생지역: string = "경기도 이천";
+
+let favoriteSong: { singer: string; song: string } = {
+  singer: "장범준",
+  song: "고백",
+};
+
+let project: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
