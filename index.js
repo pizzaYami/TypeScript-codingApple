1d48672eabe43f12c08ba0662b85913be105295d
pizzaYami <cyj9407@naver.com>
2023-08-17 09:35:18 +0900

    [feat] 추가 html

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-var 문자 = "kim";
-var 숫자 = 50;
-var 참거짓 = true;
-var 배열1 = [1, 2, 3];
-var 배열2 = ["a", "b", "c"];
-var 배열3 = [1, "a", 2, "b"];
-var 객체1 = { 사과: "apple", 포도: "grape" };
-var 객체2 = { age: 14, koreaAge: 16 };
-var 이름 = "조용주";
-var 나이 = 29;
-var 출생지역 = "경기도 이천";
-var favoriteSong = {
-    singer: "장범준",
-    song: "고백",
-};
-var project = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
-};
-var 학교 = {
-    score: [100, 97, 84],
-    teacher: "Phil",
-    friend: "John",
-};
-학교.score[4] = false;
-학교.friend = ["Lee", 학교.teacher];
+function hello(name) {
+    if (name) {
+        return "\uC548\uB155\uD558\uC138\uC694 ".concat(name, "\uC785\uB2C8\uB2E4.");
+    }
+    else {
+        return "이름이 없습니다.";
+    }
+}
+hello();
--- index.ts
+++ index.ts
@@ -1,41 +1,9 @@
-let 문자: string = "kim";
-let 숫자: number = 50;
-let 참거짓: boolean = true;
+function hello(name?: string): string {
+  if (name) {
+    return `안녕하세요 ${name}입니다.`;
+  } else {
+    return "이름이 없습니다.";
+  }
+}
 
-let 배열1: number[] = [1, 2, 3];
-let 배열2: string[] = ["a", "b", "c"];
-let 배열3: (number | string)[] = [1, "a", 2, "b"];
-
-let 객체1: { 사과: string; 포도: string } = { 사과: "apple", 포도: "grape" };
-let 객체2: { age: number; koreaAge: number } = { age: 14, koreaAge: 16 };
-
-let 이름: string = "조용주";
-let 나이: number = 29;
-let 출생지역: string = "경기도 이천";
-
-let favoriteSong: { singer: string; song: string } = {
-  singer: "장범준",
-  song: "고백",
-};
-
-let project: {
-  member: string[];
-  days: number;
-  started: boolean;
-} = {
-  member: ["kim", "park"],
-  days: 30,
-  started: true,
-};
-
-let 학교: {
-  score: (number | boolean)[];
-  teacher: string;
-  friend: string[] | string;
-} = {
-  score: [100, 97, 84],
-  teacher: "Phil",
-  friend: "John",
-};
-학교.score[4] = false;
-학교.friend = ["Lee", 학교.teacher];
+hello();
