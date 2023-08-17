d4e28228a1d259835c0aec70878993885272bf86
pizzaYami <cyj9407@naver.com>
2023-08-17 19:07:44 +0900

    [feat] 추가 객체 typealias
    (숙제1) 위 코드에서 회원정보라는 변수에 타입지정 알아서 해보십시오.
    
    - plusOne이라는 속성은 함수여야하고, 숫자를 넣어서 숫자를 뱉는 함수여야합니다.
    
    - changeName이라는 속성은 함수여야하고, 아무것도 return하면 안됩니다.
    
    - type 키워드를 쓰든 말든 알아서 합시다.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function Subject(item) {
-    if (typeof item.subject === "string") {
-        return item.subject;
-    }
-    else if (typeof item.subject === "object") {
-        return String(item.subject.slice(-1));
-    }
-    else {
-        return undefined;
-    }
-}
-console.log(Subject({ subject: "math" }));
-console.log(Subject({ subject: ["science", "art", "korean"] }));
-console.log(Subject({ hello: "hi" }));
+var 회원정보 = {
+    name: "kim",
+    age: 30,
+    plusOne: function (x) {
+        return x + 1;
+    },
+    changeName: function () {
+        console.log("안녕");
+    },
+};
+console.log(회원정보.plusOne(1));
+console.log(회원정보.changeName());
--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
-function Subject(item: { subject: string | string[] }) {
-  if (typeof item.subject === "string") {
-    return item.subject;
-  } else if (typeof item.subject === "object") {
-    return String(item.subject.slice(-1));
-  } else {
-    return undefined;
-  }
-}
-console.log(Subject({ subject: "math" }));
-console.log(Subject({ subject: ["science", "art", "korean"] }));
-console.log(Subject({ hello: "hi" }));
+type Profile = {
+  name: string;
+  age: number;
+  plusOne: (number) => number;
+  changeName: () => void;
+};
+
+let 회원정보: Profile = {
+  name: "kim",
+  age: 30,
+  plusOne(x) {
+    return x + 1;
+  },
+  changeName() {
+    console.log("안녕");
+  },
+};
+console.log(회원정보.plusOne(1));
+console.log(회원정보.changeName());
