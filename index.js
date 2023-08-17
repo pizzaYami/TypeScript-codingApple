1968946e30adcb2c0fc8ad547a93783b92e742b8
pizzaYami <cyj9407@naver.com>
2023-08-17 11:18:48 +0900

    [feat] 추가 cleaning 함수
    배열에 글자가 들어오면 숫자로 바꾸어주는 함수

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function marry(월급, 집, 매력) {
-    var point = 0;
-    point += 월급;
-    if (집 === true) {
-        point += 500;
-    }
-    if (매력 === "상") {
-        point += 100;
-    }
-    if (point >= 600) {
-        return "결혼가능";
-    }
+function cleaning(arr) {
+    var result = [];
+    arr.map(function (a) {
+        if (typeof a === "string") {
+            result.push(Number(a));
+        }
+        else {
+            result.push(a);
+        }
+    });
+    return result;
 }
-console.log(marry(300, false, "중"));
+console.log(cleaning(["122", 123, "3"]));
