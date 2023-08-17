9fd1317133e54d0d199d6def3975e7a5a3676cc0
pizzaYami <cyj9407@naver.com>
2023-08-17 09:47:13 +0900

    [feat] 수정 글자수 세주는 함수

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 function sayHi(x) {
-    if (typeof x === "string") {
-        console.log(x.length);
-    }
-    else if (typeof x === "number") {
-        console.log(String(x).length);
-    }
+    return x.toString().length;
 }
-sayHi("123");
-sayHi(123);
--- index.ts
+++ index.ts
@@ -1,10 +1,3 @@
-function sayHi(x: string | number): void {
-  if (typeof x === "string") {
-    console.log(x.length);
-  } else if (typeof x === "number") {
-    console.log(String(x).length);
-  }
+function sayHi(x: string | number): number {
+  return x.toString().length;
 }
-
-sayHi("123");
-sayHi(123);
