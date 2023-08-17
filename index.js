ecf3c066b442033402ec7816c2b42fc21d349666
pizzaYami <cyj9407@naver.com>
2023-08-17 09:54:47 +0900

    [feat] 추가 결혼가능여부

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function sayHi(x) {
-    return x.toString().length;
+function marry(월급, 집, 매력) {
+    var point = 0;
+    point += 월급;
+    if (집 === true) {
+        point += 500;
+    }
+    if (매력 === "상") {
+        point += 100;
+    }
+    if (point >= 600) {
+        return "결혼가능";
+    }
 }
+console.log(marry(300, false, "중"));
--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-function sayHi(x: string | number): number {
-  return x.toString().length;
+function marry(월급: number, 집: boolean, 매력: string): string {
+  let point = 0;
+  point += 월급;
+  if (집 === true) {
+    point += 500;
+  }
+  if (매력 === "상") {
+    point += 100;
+  }
+  if (point >= 600) {
+    return "결혼가능";
+  }
 }
+console.log(marry(300, false, "중"));
