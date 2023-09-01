0d896e5165de572c7c9dc113611ed253b7bcf51c
pizzaYami <cyj9407@naver.com>
2023-09-01 21:18:54 +0900

    [feat] class 숙제

--- index.ts
+++ index.ts
@@ -1,23 +1,15 @@
-function MathFloor(...a: number[]) {
-  return a.sort().slice(-1);
+class User {
+  private static x = 10;
+  public static y = 20;
+
+  static addOne(파라미터: number) {
+    User.x += 파라미터;
+  }
+
+  static printX() {
+    console.log(User.x);
+  }
 }
-
-function 함수1({
-  user,
-  comment,
-  admin,
-}: {
-  user: string;
-  comment: number[];
-  admin: boolean;
-}): void {
-  console.log(user, comment, admin);
-}
-
-함수1({ user: "kim", comment: [3, 5, 4], admin: false });
-
-function 함수2([a, b, c]: [a: number, b: string, c: boolean]): void {
-  console.log(a, b, c);
-}
-
-함수2([40, "wine", false]);
+User.addOne(3);
+User.addOne(10);
+User.printX();
